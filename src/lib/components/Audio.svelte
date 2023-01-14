0528b48c070286e27886ff2d3aacb1fcb3c0311a
mkin <32333141+Makohan@users.noreply.github.com>
2023-01-14 10:12:32 +0900

    refactor: audio分離

---
+++ src/lib/components/Audio.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+	let audioOk: HTMLAudioElement;
+	let audioError: HTMLAudioElement;
+
+	export const playAudioOk = () => audioOk.play();
+	export const playAudioError = () => audioError.play();
+</script>
+
+<audio bind:this={audioOk}>
+	<source src="./audio/ok.mp3" type="audio/mp3" />
+</audio>
+
+<audio bind:this={audioError}>
+	<source src="./audio/error.mp3" type="audio/mp3" />
+</audio>
